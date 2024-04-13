048c6f712fa0741f15e72cbc3fe39adaede5c78e
Zeyad Diaa <zeyaddiaa21@gmail.com>
2024-04-13 13:36:40 +0200

    Time: 33 ms (5.39%) | Memory: 38.1 MB (8.41%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,14 +4,13 @@ public:
         vector<int> output(nums.size());
         int prefix = 1, postfix = 1;
         // build prefix
-        output[0] = prefix;
+        output[0] = 1;
         for (int i = 1; i < nums.size(); i++){
             prefix *= nums[i - 1];
             output[i] = prefix;
         }
 
         // build postfix
-        output[nums.size() - 1] *= postfix;
         for( int i = nums.size() - 2; i >= 0; i--){
             postfix *= nums[i + 1];
             output[i] *= postfix;
