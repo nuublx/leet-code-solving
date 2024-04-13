4e700be612aceba427fe3e1f616ef7968c3de4be
Zeyad Diaa <zeyaddiaa21@gmail.com>
2024-04-13 12:44:13 +0200

    Time: 17 ms (16.81%) | Memory: 17.8 MB (17.13%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,23 +1,49 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int, int> mp;
-        for (int i = 0; i < nums.size(); i++){
+        vector<vector<int>> counting_array(nums.size() + 1);
+        map<int,int> mp;
+        for(int i = 0; i < nums.size(); i++){
             mp[nums[i]]++;
         }
-        priority_queue<pair<int, int>> pq;
-        for (auto& i : mp ){
-            pair<int,int> pr;
-            pr.first = i.second;
-            pr.second = i.first;
-            pq.push(pr);
+
+        for(auto& i : mp){
+            counting_array[i.second].push_back(i.first);
         }
         vector<int> results;
-        
-        for(int i = 0; i < k; i++){
-            results.push_back(pq.top().second);
-            pq.pop();
+        for(int i = nums.size(); i>= 0 && k > 0; i--){
+            if(counting_array[i].empty()){
+                continue;
+            }
+            for(int j = 0; j < counting_array[i].size() && k > 0; j++){
+                results.push_back(counting_array[i][j]);
+                k--;
+            }
         }
         return results;
+
+        /* solution number 1
+         map<int, int> mp;
+         for (int i = 0; i < nums.size(); i++){
+             mp[nums[i]]++;
+         }
+         priority_queue<pair<int, int>> pq;
+         for (auto& i : mp ){
+             pair<int,int> pr;
+             pr.first = i.second;
+             pr.second = i.first;
+             pq.push(pr);
+         }
+         vector<int> results;
+        
+         for(int i = 0; i < k; i++){
+             results.push_back(pq.top().second);
+             pq.pop();
+         }
+         return results; */
+
+
+
+    
     }
 };
